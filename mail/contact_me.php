c89ebb26811131b13109b3a1ae600ac5ec9c485d
Kirby Kohlmorgen <kirby@purdue.edu>
2015-01-28 21:17:10 -0500

    Add tutor list

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'kirby@purdue.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'usb@cs.purdue.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
