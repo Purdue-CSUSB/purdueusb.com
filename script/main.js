b4f23e09da93ee79fa9c8201d2925588cd5be773
Ian Zanger <izanger@purdue.edu>
2017-08-24 23:31:16 -0400

    Correct Ahad's label and make Rohan sound more important

--- script/main.js
+++ script/main.js
@@ -26,7 +26,7 @@ var team = [
     },
     {
         name: 'Ahad Sagheer',
-        role: 'Help Room & CS190',
+        role: 'Help Room & CS193',
         img: 'ahad'
     },
     {
@@ -76,7 +76,7 @@ var team = [
     },
     {
         name: 'Rohan Gupta',
-        role: '',
+        role: 'Student Outreach',
         img: 'rohan'
     },
     {
