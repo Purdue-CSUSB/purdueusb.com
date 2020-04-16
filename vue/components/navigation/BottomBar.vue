73615ce22e233a44f8f91cea47be27f1cb66c21d
Zach Bryant <zbryant832@gmail.com>
2020-04-15 22:13:03 -0400

    no frameworks

--- vue/components/navigation/BottomBar.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-    
-</template>
-<script>
-export default {
-  name: 'bottombar'
-}
-</script>
-<style lang="scss"></style>
--- vue/deploy.sh
+++
@@ -1,4 +0,0 @@
-cd vue
-npm run build
-npm run generate
-npm run start 
