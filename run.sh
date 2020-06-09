dfa12f0efdce8bcbdbb9548f714c2e143d924ab6
Zach Bryant <zbryant832@gmail.com>
2020-06-09 01:17:09 -0400

    cleaning echo in clean.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Cleaning cache and generated files..." && ./clean.sh
+./clean.sh
 [[ $1 != -h ]] && echo "Opening site in browser" && ./opensite.sh &
 bundle exec jekyll serve --incremental --livereload
