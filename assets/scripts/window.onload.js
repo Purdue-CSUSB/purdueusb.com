18b0629e708f90691c3d3144e675cbd9d4dcb30b
Zach Bryant <zbryant832@gmail.com>
2020-06-15 01:14:15 -0700

    sync issue

--- assets/scripts/window.onload.js
+++
@@ -1,5 +0,0 @@
-/* Make buttons act like links without the bs */
-window.addEventListener("load", finalizeButtons);
-window.addEventListener("load", scrollTop);
-window.addEventListener("load", closeFab);
-window.addEventListener("load", closeFab);
