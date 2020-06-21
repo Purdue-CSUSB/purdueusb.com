a6033f070242929b21b503df2c139e697ae839e0
Zach Bryant <zbryant832@gmail.com>
2020-06-21 16:38:44 -0700

    production and live reload modes

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
 ./clean.sh
-[[ $1 != -h ]] && echo "Opening site in browser" && ./opensite.sh &
-bundle exec jekyll serve --incremental --livereload
+
+JENV="development"
+OPEN=true
+LIVE=false
+while getopts "hpl" opt; do
+    case "${opt}" in
+        h) OPEN=false 
+        ;;
+        p) JENV="production" 
+        ;;
+        l) LIVE=true 
+        ;;
+    esac
+done
+echo Running in $JENV mode.
+$OPEN && echo "Opening site in browser" && ./opensite.sh &
+if [ $LIVE = true ]; then
+    JEKYLL_ENV=${JENV} bundle exec jekyll serve --incremental --livereload
+else
+    JEKYLL_ENV=${JENV} bundle exec jekyll serve
+fi
