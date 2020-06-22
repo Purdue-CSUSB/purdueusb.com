5a99cb1a065351c729289c5f69d280fb44a80cf2
Zach Bryant <zbryant832@gmail.com>
2020-06-21 17:23:26 -0700

    extractor for pseudoclasses, aimed at _site files

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     // These are the files that Purgecss will search through
-    content: ["./_site/**/*.html"],
+    content: ["./_site/**/*.md", "./_site/**/*.html"],
   
     // These are the stylesheets that will be subjected to the purge
-    css: ["./_site/assets/main.css"] 
+    css: ["./_site/assets/main.css"],
+
+    // For media queries, pseudo-classes, etc
+    defaultExtractor: content => content.match(/[\w-/:@]+(?<!:)/g) || []
   };
