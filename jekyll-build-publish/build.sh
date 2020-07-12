ecf08d3237fa0128dc779fd8f07f4121f345a10d
Zach Bryant <zdb282@gmail.com>
2020-07-12 01:47:07 -0700

    don't bundle as root

--- jekyll-build-publish/build.sh
+++ jekyll-build-publish/build.sh
@@ -4,6 +4,12 @@ echo "Node $(node --version)"
 echo "Ruby $(ruby -v), RubgyGems $(gem -v)"
 echo "GCC $(gcc -v), Make $(make -v)"
 
+echo NPM dependencies...
+npm i -g --unsafe-perm now
+
+echo Installing gems...
+bundle install
+
 echo Cleaning cache and generated files...
 rm -rf ./_site ./.jekyll-cache
 
