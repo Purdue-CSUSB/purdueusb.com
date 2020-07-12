6807a073a0a368a4678cab0413198733dbcdc39b
Zach Bryant <zdb282@gmail.com>
2020-07-12 01:25:47 -0700

    gh-pages build action

---
+++ jekyll-build-publish/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Node $(node --version)"
+echo "Ruby $(ruby -v), RubgyGems $(gem -v)"
+echo "GCC $(gcc -v), Make $(make -v)"
+
+echo Cleaning cache and generated files...
+rm -rf ./_site ./.jekyll-cache
+
+echo Installing gems...
+bundle install
+
+echo Building site...
+bundle exec jekyll build --trace
+
+echo Committing...
+git add -A
+git commit -m "Automatic build"
+
+echo Done
